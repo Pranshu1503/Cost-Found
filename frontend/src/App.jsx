6ab1b74d08ccb647b0d448538a3a005303716cb9
Pranshu Sharma <sharmapranshu15@gmail.com>
2025-04-12 16:21:57 +0530

    deleted app.css and index.css plus modified App.jsx and main.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,42 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
-import Welcome from "./components/Welcome";
-import Seller from "./components/Seller";
-import Buyer from "./components/Buyer";
-import Lost from "./components/Lost";
-import Find from "./components/Find"; 
-import Footer from "./components/Footer"; 
+"use client"
+
+import { useState } from "react"
+import { Routes, Route } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+import HomePage from "./pages/HomePage"
+import LostFoundPage from "./pages/LostFoundPage"
+import MarketplacePage from "./pages/MarketplacePage"
+import RentalPage from "./pages/RentalPage"
+import ItemDetailPage from "./pages/ItemDetailPage"
+import ProfilePage from "./pages/ProfilePage"
+import LoginPage from "./pages/LoginPage"
+import RegisterPage from "./pages/RegisterPage"
+import { UserContext } from "./context/UserContext"
 
 function App() {
+  const [user, setUser] = useState(null)
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Welcome />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/sell" element={<Seller />} />
-        <Route path="/buy" element={<Buyer />} />
-        <Route path="/lose" element={<Lost />} />
-        <Route path="/find" element={<Find />} />
-        
-      </Routes>
-      <Footer />
-    </Router>
-  );
+    <UserContext.Provider value={{ user, setUser }}>
+      <div className="app">
+        <Navbar />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/lost-found" element={<LostFoundPage />} />
+            <Route path="/marketplace" element={<MarketplacePage />} />
+            <Route path="/rentals" element={<RentalPage />} />
+            <Route path="/item/:id" element={<ItemDetailPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </UserContext.Provider>
+  )
 }
 
-export default App;
+export default App
